6e25222c67040824a703c034bcf2a707c2fd8f07
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-06-29 22:33:38 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react";
+
+function App() {
+
+    let post = '우동맛집';
+    let [글제목, 글제목변경] = useState(['남자 코트 추천', '우동 맛집', '여자 바지 추천']);
+    let [logo, setLogo] = useState('ReactBlog');
+    let [따봉, 따봉변경] = useState(0);
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>{logo}</h4>
+            </div>
+
+            <button onClick={() => {
+                let copy = [...글제목];
+                copy[0] = '여자 코트 추천';
+                글제목변경(copy);
+            }}>글수정</button>
+
+            <button onClick={()=>{
+                let newArray = [...글제목];
+                newArray = newArray.sort();
+                글제목변경(newArray);
+            }}>정렬</button>
+
+            <div className='list'>
+                <h4>{글제목[0]}<span onClick={() => {
+                    따봉변경(따봉 + 1)
+                }}> ❣️</span> {따봉} </h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{글제목[1]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{글제목[2]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
